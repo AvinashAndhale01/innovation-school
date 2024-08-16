4473aad3f8338bc9f1d0e186054eb479ed4a7e1f
avinashandhale01 <avinashandhale999@gmail.com>
2024-08-16 13:07:46 +0530

    Update styles and responsiveness for multiple components

--- src/components/customNavbar/Navbar.jsx
+++ src/components/customNavbar/Navbar.jsx
@@ -1,25 +1,69 @@
+import { useState, useEffect, useRef } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { logo } from "../../assets/icons";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const menuRef = useRef();
 
-  
+  const toggleMobileMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
+  // Close menu when clicking outside (only on mobile)
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        isMobile &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target)
+      ) {
+        setIsMobile(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isMobile]);
+
   return (
     <div className="nav">
-      <nav className="navbar">
+      <nav className="navbar" ref={menuRef}>
         <div className="company-logo">
           <img src={logo} alt="innovation school" />
         </div>
-        <div className="menu-list">
+
+        {/* Hamburger icon for mobile screens */}
+        <div className="hamburger" onClick={toggleMobileMenu}>
+          {isMobile ? <FaTimes /> : <FaBars />}
+        </div>
+
+        {/* Navigation links, visible on larger screens or when mobile menu is open */}
+        <div className={`menu-list ${isMobile ? "mobile-menu" : ""}`}>
           <ul>
-            <Link to="/" className="li">
+            <Link
+              to="/"
+              className="li"
+              onClick={() => isMobile && toggleMobileMenu()}
+            >
               Home
             </Link>
-            <Link to="/course" className="li">
+            <Link
+              to="/course"
+              className="li"
+              onClick={() => isMobile && toggleMobileMenu()}
+            >
               Courses
             </Link>
-            <Link to="/about" className="li">
+            <Link
+              to="/about"
+              className="li"
+              onClick={() => isMobile && toggleMobileMenu()}
+            >
               About
             </Link>
           </ul>
